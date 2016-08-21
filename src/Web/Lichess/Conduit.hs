1ba65aee3e15d579155887e93482e027723f847d
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-08-21 21:39:36 +0100

    tournament standings source

--- src/Web/Lichess/Conduit.hs
+++ src/Web/Lichess/Conduit.hs
@@ -1,4 +1,5 @@
-module Web.Lichess.Conduit (userGames) where
+module Web.Lichess.Conduit (userGames,
+                            tournamentStandings) where
 
   import Data.Aeson
   import Data.Conduit
@@ -8,10 +9,22 @@ module Web.Lichess.Conduit (userGames) where
   userGames :: String -> Source IO Value
   userGames userName = L.unfoldM (getGamesPage userName) 1 =$= L.concat
 
+  tournamentStandings :: String -> Source IO Value
+  tournamentStandings tournamentId =
+     L.unfoldM (getTournamentStandingsPage tournamentId) 1 =$= L.concat
+
   getGamesPage :: String -> Int -> IO (Maybe ([Value], Int))
   getGamesPage userName page = do
     gamesResult <- getUserGames userName page
     case gamesResult of
-      Left err -> error err
+      Left jsonErr -> error jsonErr
       Right [] -> return Nothing
       Right games -> return (Just (games, succ page))
+
+  getTournamentStandingsPage :: String -> Int -> IO (Maybe ([Value], Int))
+  getTournamentStandingsPage tournamentId page = do
+    tournamentResult <- getTournamentStandings tournamentId page
+    case tournamentResult of
+      Left jsonErr -> error jsonErr
+      Right [] -> return Nothing
+      Right standings -> return (Just (standings, succ page))
