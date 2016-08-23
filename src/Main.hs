1a51f36a2b57533ec1895791ca914e781439d0ab
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-08-23 09:23:33 +0100

    Flatten JSON structures which should make it easier to export to CSV

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,12 @@
 module Main where
+  import qualified Data.ByteString.Lazy as BL
+  import Data.Aeson
+  import Data.Conduit
+  import qualified Data.Conduit.List as CL
   import Web.Lichess.Conduit
+  import Web.Lichess.Json
   import Web.Lichess.Request
 
   main :: IO ()
-  main = putStrLn "Hello, Haskell!"
+  main = do
+    userGames "happy0" =$= CL.isolate 1 =$= CL.map flattenValue $$ CL.mapM_ (BL.putStrLn . encode)
