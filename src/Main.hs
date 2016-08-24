c381a298266699de3e4e91b9b04c046ca753b594
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-08-24 23:31:23 +0100

    conduited oot ma nut.

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,28 @@ module Main where
   import qualified Data.ByteString.Lazy as BL
   import Data.Aeson
   import Data.Conduit
+  import qualified Data.List as L
+  import qualified Data.HashMap.Strict as HM
   import qualified Data.Conduit.List as CL
+  import qualified Data.Set as S
+  import qualified Data.Text as T
   import Web.Lichess.Conduit
   import Web.Lichess.Json
   import Web.Lichess.Request
 
   main :: IO ()
   main = do
-    userGames "happy0" =$= CL.isolate 1 =$= CL.map flattenValue $$ CL.mapM_ (BL.putStrLn . encode)
+    let userGamesConduit = userGames "happy0"
+    processHeaders userGamesConduit
+
+  getKeys :: Value -> S.Set T.Text
+  getKeys (Object o) = S.fromList (HM.keys o)
+  getKeys _ = S.empty
+
+  {-
+    Helper function to work out all the valid headers for the CSV output
+  -}
+  processHeaders conduit = do
+    headers <- conduit =$= CL.isolate 200 =$= CL.map flattenValue =$= CL.map getKeys $$ CL.fold S.union S.empty
+    let headersList = S.toList headers
+    print headersList
--- src/Web/Lichess/Json.hs
+++ src/Web/Lichess/Json.hs
@@ -1,16 +1,19 @@
 module Web.Lichess.Json (flattenValue) where
 
-import Data.Aeson
+import qualified Data.Aeson as A
 import Data.Aeson.Types
+import Data.Csv
 import qualified Data.HashMap.Strict as Hm
 import Data.List
 import qualified Data.Text as T
+
+instance ToRecord Value where
+  toRecord blah = undefined
 
 flattenValue :: Value -> Value
 flattenValue value =
   let keyValues = flatten [] value
   in object keyValues
-
 
 flatten :: [T.Text] -> Value -> [Pair]
 flatten parentKeys (Object o) =
@@ -19,7 +22,7 @@ flatten parentKeys (Object o) =
     pairs <- flatten (parentKeys ++ [key]) value
     return pairs
 
-flatten keys@(parentKey : rest) value = [(concatKeys keys) .= value]
+flatten keys@(parentKey : rest) value = [(concatKeys keys) A..= value]
 flatten _ _ = error "I dun goofed."
 
 concatKeys :: [T.Text] -> T.Text
